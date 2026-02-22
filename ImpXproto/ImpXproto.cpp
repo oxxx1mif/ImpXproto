710944752f1a7746c4649e32bc62324a0601d85f
oxxx1mif <pluginexteragram@gmail.com>
2026-02-22 19:18:29 +1000

    Добавьте файлы проекта.

---
+++ ImpXproto/ImpXproto.cpp
@@ -0,0 +1,14 @@
+﻿/**
+ * This is the source code of ImpXproto for Impergram & ImpergramX & GuA & Imperium.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ * Please, be respectful and credit the original author if you use this code.
+ *
+ * Copyright Obitocjkiy Gleb & github.com/oxxx1mif, 2026.
+ */
+
+#include <iostream>
+
+int main() {
+	return 0;
+}
---
+++ ImpXproto/ImpXproto.h
@@ -0,0 +1,8 @@
+﻿/**
+ * This is the source code of ImpXproto for Impergram & ImpergramX & GuA & Imperium.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ * Please, be respectful and credit the original author if you use this code.
+ *
+ * Copyright Obitocjkiy Gleb & github.com/oxxx1mif, 2026.
+ */
