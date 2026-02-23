82c24200b93a35a1691ea165ad9445d3896c5551
oxxx1mif <pluginexteragram@gmail.com>
2026-02-23 11:52:46 +1000

    update

--- ImpXproto/ImpXproto.h
+++ ImpXproto/ImpXproto.h
@@ -1,8 +1,19 @@
-﻿/**
- * This is the source code of ImpXproto for Impergram & ImpergramX & GuA & Imperium.
- * It is licensed under GNU GPL v. 2 or later.
- * You should have received a copy of the license in this archive (see LICENSE).
- * Please, be respectful and credit the original author if you use this code.
- *
- * Copyright Obitocjkiy Gleb & github.com/oxxx1mif, 2026.
- */
+﻿/*
+    This file is part of ImpXproto Library.
+
+    ImpXproto Library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    ImpXproto Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with ImpXproto Library.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2026 Impergram & ImpergramX & GuA development team
+              2026 Obitocjkiy Gleb
+*/
